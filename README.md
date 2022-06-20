298cd28a51dad12c2d2bbc71272d49bffb968024
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-06-20 02:51:58 +0000

    Update the readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # What is this project?
 This is Spork. A multipurpose megatool that exposes a certain kind of workflow to enable quickly putting together the UI and API of a project.
+
+## How can I setup Spork to use as a template for my stuff?
+Honestly, I'd recommend cloning the repo via git or downloading the zipfile.
+
+```
+git clone git@github.com:spork-app/app
+git remote rm origin
+```
+
+From there, you can use the install script
+
+```
+bash install.sh
+```
+
+You'll be prompted for installing available packages with the `spork-app` tag through composer.
+
+Any custom packages can be added to Spork via [custom repositories through composer](https://getcomposer.org/doc/05-repositories.md#vcs).
+
+And assets can be included directly in your `resources/js/app.js` file
+
+## How can I make my own plugin?
+There's a package for that :smile: https://github.com/spork-app/template-plugin. It's templated with the keys of [the spork.json file](https://github.com/spork-app/template-plugin/blob/main/spork.json), and it's intended to be used with the spork/development package, which will automatically prompt for and replace values in the `spork.json` file.
--- install.sh
+++ install.sh
@@ -12,14 +12,9 @@ sed -i "s/PUSHER_APP_SECRET=/PUSHER_APP_SECRET=$PUSHER_APP_SECRET/g" .env
 
 # Install composer
 if [ -f `which docker` ]; then 
-    docker run --rm -v "$(pwd)":/opt -w /opt laravelsail/php81-composer:latest bash -c "composer install"
+    docker run --rm -v "$(pwd)":/opt -w /opt austinkregel/base:latest bash -c "composer install"
 else
     composer install
-fi
-
-if [[ "$(which sail)" = "" ]]; then
-    export alias sail="$(pwd)/vendor/bin/sail"
-    echo 'alias sail="$(pwd)/vendor/bin/sail"' >> ~/.bashrc
 fi
 
 vendor/bin/sail up -d
@@ -27,70 +22,3 @@ vendor/bin/sail up -d
 vendor/bin/sail artisan key:generate
 
 vendor/bin/sail artisan install
-# "spork/analytics": "dev-main",
-# "spork/calendar": "dev-main",
-# "spork/development": "dev-main",
-# "spork/finance": "dev-main",
-# "spork/food": "dev-main",
-# "spork/greenhouse": "dev-main",
-# "spork/maintenance": "dev-main",
-# "spork/news": "dev-main",
-# "spork/planning": "dev-main",
-# "spork/research": "dev-main",
-# "spork/shopping": "dev-main",
-# "spork/weather": "dev-main",
-# "spork/wiretap": "dev-main",
-
-
-# {
-#     "type": "path",
-#     "url": "./system/analytics"
-# },
-# {
-#     "type": "path",
-#     "url": "./system/calendar"
-# },
-# {
-#     "type": "path",
-#     "url": "./system/development"
-# },
-# {
-#     "type": "path",
-#     "url": "./system/finance"
-# },
-# {
-#     "type": "path",
-#     "url": "./system/food"
-# },
-# {
-#     "type": "path",
-#     "url": "./system/maintenance"
-# },
-# {
-#     "type": "path",
-#     "url": "./system/news"
-# },
-# {
-#     "type": "path",
-#     "url": "./system/planning"
-# },
-# {
-#     "type": "path",
-#     "url": "./system/research"
-# },
-# {
-#     "type": "path",
-#     "url": "./system/greenhouse"
-# },
-# {
-#     "type": "path",
-#     "url": "./system/shopping"
-# },
-# {
-#     "type": "path",
-#     "url": "./system/weather"
-# },
-# {
-#     "type": "path",
-#     "url": "./system/wiretap"
-# }

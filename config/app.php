0db1e6d8d9329667cf3e6d56c77cc4e2aa4fca01
austinkregel <austinkregel@users.noreply.github.com>
2022-11-24 07:16:51 +0000

    PHP Linting (Pint)

--- config/app.php
+++ config/app.php
@@ -192,7 +192,7 @@ return [
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
-//        App\Providers\TelescopeServiceProvider::class,
+        //        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
         // Spork service providers _should_ be autoregistered, but any that aren't can be here.
